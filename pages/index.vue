dad8e44654e66275147a483ef4f78523f68c4ee5
AmanoKokoro <55781271+AmanoKokoro@users.noreply.github.com>
2021-11-14 16:47:27 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="container">
+    <video-player :options="videoOptions"></video-player>
+  </section>
+</template>
+
+<script>
+import VideoPlayer from '@/components/VideoPlayer.vue';
+import 'video.js/dist/video-js.css';
+
+export default {
+  components: {
+    VideoPlayer
+  },
+  data() {
+    return {
+      videoOptions: {
+        autoplay:true,
+        controls: true,
+        sources: [
+          {
+            src: '/static/hoge.webm',
+            type: 'video/webm'
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
